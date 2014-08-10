46f3ebaa191073ea2ba6c47352f153de4b379c9e
randre03 <roger.randall@gmail.com>
2014-08-10 01:01:01 -0500

    added User pages

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                   "Roger Randall"
+    email                  "roger.randall@gmail.com"
+    password               "redsox9"
+    password_confirmation  "redsox9"
+  end
+end
